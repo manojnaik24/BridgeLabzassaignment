7c6bd2bc26291271e6fc874648ac7cf1bb3e3213
Manoj naik <chittinaik@gamil.com>
2023-09-04 14:08:25 +0530

    [Manoj] add:Head or Tail

---
+++ BridgeLabzAssaignment/BridgeLabzAssaignment/Flip Coin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzAssaignment
+{
+    internal class Flip_Coin
+    {
+
+       public static void Flip(int n)
+        {
+            int headsCount = 0;
+            int tailsCount = 0;
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                // Use random.NextDouble() to get a random number between 0 and 1
+                if (random.NextDouble() < 0.5)
+                {
+                    headsCount++;
+                }
+                else
+                {
+                    tailsCount++;
+                }
+            }
+
+            double headsPercentage = (double)headsCount / n * 100;
+            double tailsPercentage = (double)tailsCount / n * 100;
+
+            Console.WriteLine($"Heads: {headsPercentage:F2}% ");
+            Console.WriteLine($" Tails: {tailsPercentage:F2}%");
+        }
+    }
+}
+
---
+++ BridgeLabzAssaignment/BridgeLabzAssaignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzAssaignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number: ");
+            String n=Console.ReadLine();
+            Flip_Coin.Flip(int.Parse(n));
+            Console.ReadLine();
+        }
+    }
+}
